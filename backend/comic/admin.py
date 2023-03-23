481121c14daabffc53ad67af757a30299a29265a
haphanbaominh <haphanbaominh9674@gmail.com>
2023-03-23 17:06:18 +0700

    first commit

---
+++ backend/comic/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Genre, Comic, Chap, Image
+# Register your models here.
+admin.site.register(Genre)
+# admin.site.register(Chap)
+#  return f"{self.name} {self.view} {self.chap} {self.rating} {self.status}"
+
+
+class ComicAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "view", "chap", "rating",
+                    "updated_at", "created_at", "status")
+
+
+admin.site.register(Comic, ComicAdmin)
+
+
+class ChapAdmin(admin.ModelAdmin):
+    list_display = ("chap_num", "name", "comic", "updated_at")
+
+
+admin.site.register(Chap, ChapAdmin)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("order", "image", "chap", "comic")
+
+
+admin.site.register(Image, ImageAdmin)
---
+++ backend/comic/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.http import HttpResponse
+
+urlpatterns = [
+    # path('/', views.index),
+    path('/search', views.getComicSearch),
+    path('/genres', views.getGenres),
+    path('/chap/image/<int:chap_id>', views.getChapImage),
+    path('/<str:genre_slug>', views.getComicByGenreSlug),
+    path('/detail/<int:comic_id>', views.getComicDetail),
+    path('/<str:sort_field>/<int:page_num>', views.getComicBySortFiled),
+]
